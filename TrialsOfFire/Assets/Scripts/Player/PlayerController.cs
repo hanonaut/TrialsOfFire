5165561b7fc0e4911626f9909b629a1d2aea970f
frap1011 <mokajesse@gmail.com>
2023-01-18 18:48:57 -0600

    Update PlayerController.cs
    Camera fixes

--- TrialsOfFire/Assets/Scripts/Player/PlayerController.cs
+++ TrialsOfFire/Assets/Scripts/Player/PlayerController.cs
@@ -28,10 +28,12 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         float mouse_x = Input.GetAxis("Mouse X");
+		float mouse_y = Input.GetAxis("Mouse Y");
 
-        Vector3 move = transform.forward * vertical + transform.right * horizontal;
+		Vector3 move = transform.forward * vertical + transform.right * horizontal;
 
         mageCharacter.Move(move * speed * Time.deltaTime);
         transform.Rotate(0f, mouse_x, 0f);
+        eyes.transform.Rotate(-mouse_y, 0f, 0f);
     }
 }
