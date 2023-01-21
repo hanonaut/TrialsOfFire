0f7aac85294359c9951dadfe110f4a59c07009c4
frap1011 <mokajesse@gmail.com>
2023-01-20 20:44:10 -0600

    Updating the AI
    Making basic AI

--- TrialsOfFire/Assets/Scripts/AI/Basic AI.cs
+++ TrialsOfFire/Assets/Scripts/AI/Basic AI.cs
@@ -9,13 +9,19 @@ public class BasicAI : MonoBehaviour
     private NavMeshAgent agent;
 
     //Named this way for a specific reason
-    public LayerMask playerShape;
+    public LayerMask playerShape, isTheGround;
 
     [SerializeField]
     private Transform targetPlayer;
 
     [SerializeField]
+    private float patrolRange;
+
+    [SerializeField]
     private float sightRange;
+
+    bool walkPointSet;
+    private Vector3 walkPoint;
 
     bool playerInSight;
 
@@ -36,5 +42,26 @@ public class BasicAI : MonoBehaviour
     void Update()
     {
         playerInSight = Physics.CheckSphere(transform.position, sightRange, playerShape);
+
+        if (playerInSight) Roaming();
     }
+
+    private void Roaming() {
+        if (!walkPointSet) ChooseLocation();
+
+        if (walkPointSet) agent.SetDestination(walkPoint);
+
+        if ((transform.position - walkPoint).magnitude < 1f) walkPointSet = false;
+
+
+	}
+
+    private void ChooseLocation(){
+        float randomX = Random.Range(-patrolRange, patrolRange);
+		float randomZ = Random.Range(-patrolRange, patrolRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        if(Physics.Raycast(walkPoint, -transform.up, 2f, isTheGround)) walkPointSet = true;
+
+	}
 }
