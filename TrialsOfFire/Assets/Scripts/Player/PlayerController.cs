2390821efc08c38a91c6235aca833c22ebc3091d
frap1011 <mokajesse@gmail.com>
2023-01-26 19:41:37 -0600

    Added crappy dash

--- TrialsOfFire/Assets/Scripts/Player/PlayerController.cs
+++ TrialsOfFire/Assets/Scripts/Player/PlayerController.cs
@@ -14,12 +14,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float speed;
 
+    private bool canDash;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+
+        canDash = true;
     }
 
     // Update is called once per frame
@@ -35,5 +39,18 @@ public class PlayerController : MonoBehaviour
         mageCharacter.Move(move * speed * Time.deltaTime);
         transform.Rotate(0f, mouse_x, 0f);
         eyes.transform.Rotate(-mouse_y, 0f, 0f);
+        Debug.Log("Can Dash: " + canDash);
+
+        if(Input.GetKeyDown(KeyCode.LeftShift) && canDash){
+			mageCharacter.Move(move * 900f * Time.deltaTime);
+            StartCoroutine(ReloadDash());
+        }
+    }
+
+    IEnumerator ReloadDash(){
+        canDash = false;
+        yield return new WaitForSeconds(4f);
+        canDash = true;
+
     }
 }
